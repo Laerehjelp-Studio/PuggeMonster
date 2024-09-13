c8e245c5ca8a2d95753195ad4fd25aeb522f4522
Tor-Arne Sandstrak <github@logi.pm>
2024-09-13 14:49:50 +0200

    Added TaskMaster and MathTask

---
+++ Assets/_Scripts/TaskMaster.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class TaskMaster : MonoBehaviour {
+
+
+}
+public struct MathTask {
+	public float[] Components; // Array with 2 numbers
+	public string Operator; // + - * or /
+	public float Correct; // The correct answer.
+	public float[] Incorrect; // Incorrect options.
+}
