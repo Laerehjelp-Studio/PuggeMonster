25ecb56e5450c52ff1aa07b3025e43fdc6fdd193
Tor-Arne Sandstrak <github@logi.pm>
2024-09-20 07:36:27 +0200

    Added NumberSprite variable to MathTask struct

--- Assets/_Scripts/TaskMaster.cs
+++ Assets/_Scripts/TaskMaster.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 /// 
 /// </summary>
 public class TaskMaster : MonoBehaviour {
-
-
+	/* */
 }
+
 public struct MathTask {
 	public float[] Components; // Array with 2 numbers
 	public string Operator; // + - * or /
 	public float Correct; // The correct answer.
 	public float[] Incorrect; // Incorrect options.
+	public Sprite NumberSprite;
 }
