281657bef6043627e21a7c652b640eeecdda6fc6
Tor-Arne Sandstrak <github@logi.pm>
2024-11-30 12:43:56 +0100

    You are no longer flashed the background before the GalleryScene is loaded, gallery opening & close book is updated.

--- Assets/_Scripts/BookAnimationBehaviour.cs
+++ Assets/_Scripts/BookAnimationBehaviour.cs
@@ -15,6 +15,7 @@ public class BookAnimationBehaviour : MonoBehaviour
     [SerializeField] private GameManager gameManager;
     private float calculatedWaitTime;
     private SpriteRenderer spriteImage;
+    private bool inGallery = false;
 
     private void Awake()
     {
@@ -22,16 +23,17 @@ public class BookAnimationBehaviour : MonoBehaviour
         spriteImage.enabled = false;
     }
 
-    private void OnEnable() {
-        
-        spriteImage.sprite = imageFramesList[0];
-    }
-
     public void StartBookAnimation()
     {
         if(!playAnimation)
         {
             playAnimation = true;
+        }
+    }
+
+    private void OnEnable() {
+        if (inGallery) {
+            StartCoroutine(PlayAnimationThing());
         }
     }
 
@@ -52,20 +54,28 @@ public class BookAnimationBehaviour : MonoBehaviour
     IEnumerator PlayAnimationThing()
     {
         calculatedWaitTime = 1 / framesPerSec;
-        int index = 0;
+        int index = (!inGallery) ? 0: (imageFramesList.Count - 1);
         spriteImage.sprite = imageFramesList[index];
 
-        while (spriteImage.sprite != imageFramesList[imageFramesList.Count - 1])
+        Sprite spriteTarget = (!inGallery) ? imageFramesList[imageFramesList.Count - 1]: imageFramesList[0];
+        while (spriteImage.sprite != spriteTarget)
         {
+            index = (!inGallery) ? index + 1: index - 1;
             spriteImage.sprite = imageFramesList[index];
-            index++;
             yield return new WaitForSecondsRealtime(calculatedWaitTime);
         }
-        gameManager.GalleryLoader("GalleryScene");
-        BookButton.SetActive(true);
-        BookWhiteOut.SetActive(true);
-        floatingScript.enabled = true;
-        spriteImage.enabled = false;
+
+        if (inGallery) {
+            inGallery = false;
+            spriteImage.sprite = imageFramesList[0];
+            BookButton.SetActive(true);
+            BookWhiteOut.SetActive(true);
+            floatingScript.enabled = true;
+            spriteImage.enabled = false;
+        } else {
+            gameManager.GalleryLoader("GalleryScene");
+            inGallery = true;
+        }
         StopCoroutine(PlayAnimationThing());
         Debug.Log("animation finished");
     }
